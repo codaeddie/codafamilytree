e55b926de0988a0b49ac8280df80d859b6a9f02f
codaeddie <eddiecoda24@gmail.com>
2024-11-18 13:43:00 -0800

    mermaid formatting & fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,173 @@
+'use client'
+
 import MermaidDiagram from '@/app/components/MermaidDiagram'
+
 
 export default function Home() {
 	const diagram = `
+%%{
+  init: {
+    'theme': 'dark',
+    'themeVariables': {
+      'fontFamily': 'Inter, ui-sans-serif, system-ui'
+    }
+  }
+}%%
+
+flowchart TD
+  subgraph MakdesiChildren["Makdesi Family"]
+    Besma["Besma"]
+    Sarmad["Sarmad"]
+    Mazen["Mazen"]
+    Nithal["Nithal"]
+    Moiad["Moiad"]
+    Khalid-AKA-Jerry["Khalid-AKA-Jerry"]
+    Nisreen["Nisreen"]
+    Nawal["Nawal"]
+  end
+  subgraph CodaChildren["Coda Family"]
+    Rawayda["Rawayda"]
+    Rita["Rita"]
+    Ronda["Ronda"]
+    Rafa["Rafa"]
+    Rami["Rami"]
+    Raymond["Raymond"]
+    Rafael["Rafael"]
+    Riath["Riath"]
+  end
+  subgraph MainFamily["THE *Coda* Family"]
+    direction LR
+    EdmundSargonPeter["EdmundSargonPeter"]
+    Ares["Ares"]
+    Nora["Nora"]
+  end
+  subgraph Sarmad'sFamily["Sarmad's Kids"]
+    direction LR
+    Brandon["Brandon"]
+    Ryan["Ryan"]
+    Emma["Emma"]
+  end
+  subgraph Mazin's_Family["Mazin's Kids"]
+    direction LR
+    Gina["Gina"]
+    Maryan["Marianne"]
+    Angela["Angela"]
+    Luke["Luke"]
+  end
+  subgraph Makdesi_Family["Moiad's Kids"]
+    Lena["Lena"]
+    Chris["Chris"]
+    Patrick["Patrick"]
+  end
+  subgraph Shaia_Family["lena's Kids"]
+    Alyssa["Alyssa"]
+    Gianna["Gianna"]
+    Liam["Liam"]
+  end
+  subgraph NawalFamily["The Alkatib's"]
+    Rene["Renee"]
+    Monique["Monique"]
+    Diana["Diana"]
+    Rhonda["Rhonda"]
+  end
+  subgraph MOMO["Yacoub's"]
+    direction LR
+    Nathan["Nathan"]
+    Alexander["Alexander"]
+  end
+  subgraph Romaya_Fam["Romaya's"]
+    direction LR
+    Sofia["Sofia"]
+    Dalia["Dalia"]
+    Mia["Mia"]
+    Rita.["Rita"]
+  end
+  subgraph Younan_Fam["Younan's"]
+    direction LR
+    cici["Celine"]
+    Julian["Julian"]
+    Preston["Preston"]
+  end
+  subgraph triplets["Triplets"]
+    Allison["Allison"]
+    Lauren["Lauren"]
+    Dylan["Dylan"]
+  end
+  subgraph Jouni["Jouni Family"]
+    Danny["Danny"]
+    David["David"]
+    triplets
+  end
+  subgraph Hanna["The One and Only"]
+    Natalie((("Natalie")))
+  end
+  subgraph RamiFamily["Coda's"]
+    direction LR
+    Anna["Anna"]
+    Angelina["Angelina"]
+  end
+  subgraph RaymondFamily["RefEt Coda's"]
+    Andrew["Andrew"]
+    Sarah["Sarah"]
+  end
+  subgraph RafaelFamily["RefAtt Coda's"]
+    direction LR
+    Alvin["Alvin"]
+    Rosie["Rosie"]
+    Ila["Ila"]
+  end
+  subgraph RawaydaFamily["Coda's"]
+    Rafel["Rafel"]
+    Sadda["Sadda"]
+  end
+  subgraph Rafel_Fam["Coda's"]
+    Reagen["Reagen"]
+    Royal["Royal"]
+    Roman["Roman"]
+    Sophia["Sophia"]
+  end
+  subgraph RitaFamily["Glina's"]
+    Alen["Alen"]
+    Tara["Tara"]
+  end
+  subgraph Glina["Glina's"]
+    Clara["Clara"]
+    Thomas["Thomas"]
+    Ben["Ben"]
+  end
+  subgraph RondaFamily["Ronda's kids"]
+    Nibras["Nibras"]
+    Febras["Firas"]
+  end
+  MakdesiGP(("Gido Poulus + Nana Mary")) ===> MakdesiChildren
+  CodaGP(("Gido Edmoond + Nana Salha")) ===> CodaChildren
+  Besma ==> MainFamily
+  Riath ==> MainFamily
+  Sarmad -- <br>Sarmad &amp; Saba Makdesi<br> ---> Sarmad'sFamily
+  Mazen -- <br>Mazen &amp; Linda Makdesi<br> ---> Mazin's_Family
+  Moiad -- <br>Moiad &amp; Neda Makdesi<br> ---> Makdesi_Family
+  Lena -- <br>Randy &amp; Lena Shaia<br> ----> Shaia_Family
+  Nawal -- <br> Adil &amp; Nawal Alkatib<br> ---> NawalFamily
+  Monique -- <br> Lenny &amp; Monique Yacoub<br> ----> MOMO
+  Diana -- <br> Sinan &amp; Diana Romaya<br> ----> Romaya_Fam
+  Rhonda -- <br> Sal &amp; Rhonda Younan<br> ----> Younan_Fam
+  Nisreen -- <br> Majid &amp; Nisreen Jouni<br> ---> Jouni
+  Nithal -- <br> Imad &amp; Nithal Hanna<br> ---> Hanna
+  Rami -- <br> Rami &amp; uhhhmn3 Coda<br> ---> RamiFamily
+  Raymond -- <br> Refet &amp; Nehla Coda<br> ---> RaymondFamily
+  Rafael -- <br> Rafael &amp;Rena Coda<br> ---> RafaelFamily
+  Rawayda -- <br> Salah &amp; Rawayda Coda<br> ---> RawaydaFamily
+  Rafel -- <br> Rafel &amp; D Coda<br> ----> Rafel_Fam
+  Rita -- <br> uhhhm4 &amp; Rita Glina<br> ---> RitaFamily
+  Alen -- <br> Alen &amp; Katie Glina<br> ----> Glina
+
 ---
 config:
 	theme: dark
 	look: classic
 	layout: fixed
 ---
+
 flowchart TD
  subgraph MakdesiChildren["Makdesi Family"]
         Besma["Besma"]
