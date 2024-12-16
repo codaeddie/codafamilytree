a07fe01613d7e92fc5603937eb4ee0cf9d5dc7e3
codaeddie <eddiecoda24@gmail.com>
2024-12-15 18:46:19 -0800

    vercel analytics update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 import {ThemeProvider } from './theme-provider'
 import "./globals.css";
 
@@ -29,6 +30,7 @@ export default function RootLayout({
 					{children}
 					</main>
 					<Analytics />
+					<SpeedInsights />
 				</ThemeProvider>
 			</body>
 		</html>
